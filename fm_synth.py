96c87c344a4c2dfa1cd55ef0c65a1252fc7dd1cb
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2023-02-01 20:32:16 -0800

    Added locking for fm param changes, but something is still out o synch

--- fm_synth.py
+++ fm_synth.py
@@ -5,6 +5,8 @@ See http://www.cs.cmu.edu/~music/icm-online/readings/fm-synthesis/index.html
 """
 import numpy as np
 
+from threading import Lock
+
 
 class FMSynthesizer(object):
     """
@@ -19,6 +21,7 @@ class FMSynthesizer(object):
                         'mod_depth': modulation_init[1]}
         self._new_params = self._params.copy()
         self._carrier_phase, self._mod_phase = 0.0, 0.0
+        self._update_lock = Lock()
 
     def __str__(self):
         return "FMSynth(C_f = %.3f Hz, C_a = %.2f %%, M_f = %.3f Hz. M_d = %.2f)" % (self._params['carrier_freq'],
@@ -26,12 +29,13 @@ class FMSynthesizer(object):
                                                                                      self._params['mod_freq'],
                                                                                      self._params['mod_depth'])
 
-    def set_param(self, name, value):
+    def set_params(self, updates):
         """
         Remember the previous value for smooth interpolation
         TODO:   Keep ALL previous values, tag w/timestamp, so values changing too fast aren't lost.
         """
-        self._new_params[name] = value
+        with self._update_lock:
+            self._new_params.update(updates)
 
     def reset(self):
         self._params = self._new_params.copy()
@@ -42,21 +46,26 @@ class FMSynthesizer(object):
         Get the next N samples and the time of sample n+1
         returns floats in [-1,1], or bytes
         """
-        mod_phase_increments = np.cumsum(
-            np.hstack([0, np.linspace(2. * np.pi * self._params['mod_freq'] / self._rate,
-                                      2. * np.pi * self._new_params['mod_freq'] / self._rate, n)]))
-        car_phase_increments = np.cumsum(
-            np.hstack([0, np.linspace(2. * np.pi * self._params['carrier_freq'] / self._rate,
-                                      2. * np.pi * self._new_params['carrier_freq'] / self._rate, n)]))
+        with self._update_lock:
+            params = self._params
+            new_params = self._new_params.copy()
+            if advance:
+                self._params = self._new_params.copy()
 
-        depth = np.linspace(self._params['mod_depth'], self._new_params['mod_depth'], n)
-        amp = np.linspace(self._params['carrier_amp'], self._new_params['carrier_amp'], n)
+        mod_phase_increments = np.cumsum(
+            np.hstack([0, np.linspace(2. * np.pi * params['mod_freq'] / self._rate,
+                                      2. * np.pi * new_params['mod_freq'] / self._rate, n)]))
+        car_phase_increments = np.cumsum(
+            np.hstack([0, np.linspace(2. * np.pi * params['carrier_freq'] / self._rate,
+                                      2. * np.pi * new_params['carrier_freq'] / self._rate, n)]))
+
+        depth = np.linspace(params['mod_depth'], new_params['mod_depth'], n)
+        amp = np.linspace(params['carrier_amp'], new_params['carrier_amp'], n)
 
         modulation = np.sin(self._mod_phase + mod_phase_increments[:-1]) * depth
         samples = np.sin(self._carrier_phase + car_phase_increments[:-1] + modulation) * amp
 
         if advance:
-            self._params = self._new_params.copy()
             self._carrier_phase += car_phase_increments[-1]  # np.mod(car_phase_increments[-1], np.pi * 2.0)
             self._mod_phase += mod_phase_increments[-1]  # np.mod(mod_phase_increments[-1], np.pi * 2.0)
 
